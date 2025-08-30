1d3c1fa06200519d1ce98ce6b2ed4ab00658b6a8
Marcus Faria <mfaria@ness.com.br>
2025-08-30 15:14:01 -0300

    feat(homepage): Change style of Checkboxes and Button from form.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,31 +50,56 @@ export default function SurveyForm() {
             <p>{q.id}) {q.text}</p>
             <div className={styles.options}>
               {q.options.map((opt, i) => (
-                <div key={i} className={styles.checkbox}>
-                  <input
-                    type="checkbox"
+                <div key={i} className={styles.checkboxSketch}>
+                  <input 
+                    type="checkbox" 
                     id={`morning-${q.id}-${i}`}
+                    className={styles.check}
                     name={q.id}
                     value={opt}
                     checked={answers[q.id] === opt}
                     onChange={(e) => handleChange(q.id, e.target.value)}
                     required
                   />
-                  <label 
+                  <label
                     key={opt}
                     htmlFor={`morning-${q.id}-${i}`}
                   >
-                    <span><svg width="12px" height="10px">
-                      <use xlinkHref="#check-4"></use>  
-                    </svg></span>
+                    <svg width="45" height="45" viewBox="0 0 95 95">
+                      <rect x="30" y="20" width="50" height="50" stroke="black" fill="none" />
+                      <g transform="translate(0,-952.36222)">
+                        <path d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 " stroke="black" strokeWidth="3" fill="none" className={styles.path1} />
+                      </g>
+                    </svg>
                     <span>{opt}</span>
                   </label>
-                  <svg className={styles.inlineSvg}>
-                    <symbol id="check-4" viewBox="0 0 12 10">
-                      <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                    </symbol>
-                  </svg>
                 </div>
+
+                // <div key={i} className={styles.checkbox}>
+                //   <input
+                //     type="checkbox"
+                //     id={`morning-${q.id}-${i}`}
+                //     name={q.id}
+                //     value={opt}
+                //     checked={answers[q.id] === opt}
+                //     onChange={(e) => handleChange(q.id, e.target.value)}
+                //     required
+                //   />
+                //   <label 
+                //     key={opt}
+                //     htmlFor={`morning-${q.id}-${i}`}
+                //   >
+                //     <span><svg width="12px" height="10px">
+                //       <use xlinkHref="#check-4"></use>  
+                //     </svg></span>
+                //     <span>{opt}</span>
+                //   </label>
+                //   <svg className={styles.inlineSvg}>
+                //     <symbol id="check-4" viewBox="0 0 12 10">
+                //       <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                //     </symbol>
+                //   </svg>
+                // </div>
               ))}
             </div>
           </div>
@@ -82,7 +107,7 @@ export default function SurveyForm() {
 
         <button
           type="submit"
-          className=""
+          className={styles.btnSubmit}
         >
           Enviar
         </button>
