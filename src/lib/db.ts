3658c83d4036413d6ea3a637dc772b99d4a0e992
Marcus Faria <mfaria@ness.com.br>
2025-08-30 14:42:09 -0300

    feat(database): Remove files of database.

--- src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-export async function getDb() {
-  if (typeof window !== "undefined") {
-    throw new Error("DB só pode ser usado no servidor");
-  }
-  const dbModule = await import("../server/db");
-  return dbModule.default;
-}
